60a3a2be283253488efb2b3c8a3c2ab537f40fcf
Michel Henrique <michelstcosta@gmail.com>
2024-02-07 18:25:07 -0300

    Atualizando dados

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -95,4 +95,18 @@ public class Medico {
     public void setEndereco(Endereco endereco) {
         this.endereco = endereco;
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+
+        if (dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if (dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
 }
