34b86ec16843ba51e08d48008d4e0d9ec2d8a296
Michel Henrique <michelstcosta@gmail.com>
2024-02-08 15:28:11 -0300

    Finalizacao do tratamento de erros

--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,6 +1,5 @@
 package med.voll.api.controller;
 
-import med.voll.api.paciente.PacienteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -1,8 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -62,8 +61,7 @@ public class MedicosController {
     @GetMapping("/{id}")
     public ResponseEntity detalhar(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
-
-
         return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
     }
 }
