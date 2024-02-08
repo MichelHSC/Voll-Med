e943fd53edd31cae70a9078be47fc7dc60200ce5
Michel Henrique <michelstcosta@gmail.com>
2024-02-08 13:37:48 -0300

    adicionando tratamento de erro 500

--- src/main/java/med/voll/api/controller/MedicosController.java
+++ src/main/java/med/voll/api/controller/MedicosController.java
@@ -58,4 +58,12 @@ public class MedicosController {
 
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
 }
