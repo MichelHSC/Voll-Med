6c6cd426e08efe1013c2aede447131560cce86f1
Michel Henrique <michelstcosta@gmail.com>
2024-02-09 11:35:53 -0300

    autenticacao de token

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -16,9 +17,13 @@ import java.util.Date;
 @Service
 public class TokenService {
 
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario){
+        System.out.println(secret);
         try {
-            var algoritmo = Algorithm.HMAC256("186523");
+            var algoritmo = Algorithm.HMAC256(secret);
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
